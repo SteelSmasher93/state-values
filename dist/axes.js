e6fa95b2c8dcede2b5407bbfdf769904beb17571
TheGhostOfInky <69942208+TheGhostOfInky@users.noreply.github.com>
2021-09-20 03:53:20 +0100

    Fixed array

--- dist/axes.js
+++ dist/axes.js
@@ -1,4 +1,4 @@
-var ptnl_array = ['Libertarian', 'Autonomist', 'Centrist', 'Paternalist-Leaning', 'Paternalist'];
+var ptnl_array = ['Paternalist', 'Paternalist-Leaning', 'Centrist', 'Autonomist', 'Libertarian'];
 var stat_array = ['Planned Economy', 'Regulationist', 'Centrist', 'Free-Market', 'Laissez-Faire'];
-var part_array = ['Partisan', 'Partisan-Leaning', 'Mixture', 'Non-Partisan Leaning', 'Non-Partisan'];
 var auth_array = ['Autocracy', 'Authoritarian Democracy', 'Mixed Regime', 'Constitutional Democracy', 'Full Democracy'];
+var part_array = ['Partisan', 'Partisan-Leaning', 'Mixture', 'Non-Partisan Leaning', 'Non-Partisan'];
--- src/axes.ts
+++ src/axes.ts
@@ -1,4 +1,4 @@
-let ptnl_array: Array<string> = ['Libertarian', 'Autonomist', 'Centrist', 'Paternalist-Leaning', 'Paternalist']
+let ptnl_array: Array<string> = ['Paternalist','Paternalist-Leaning','Centrist','Autonomist','Libertarian']
 let stat_array: Array<string> = ['Planned Economy', 'Regulationist', 'Centrist', 'Free-Market', 'Laissez-Faire']
-let part_array: Array<string> = ['Partisan', 'Partisan-Leaning', 'Mixture', 'Non-Partisan Leaning', 'Non-Partisan']
 let auth_array: Array<string> = ['Autocracy', 'Authoritarian Democracy', 'Mixed Regime', 'Constitutional Democracy', 'Full Democracy']
+let part_array: Array<string> = ['Partisan', 'Partisan-Leaning', 'Mixture', 'Non-Partisan Leaning', 'Non-Partisan']
